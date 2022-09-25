98bb767ed9b7f0cc668bf1bf60e3892e50db2e11
Falase Femi <femifalase228@gmail.com>
2022-09-25 16:01:11 +0100

    first push

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='max-w-[1180px] pt-[48px] pb-[60px] px-[40px]'>
+        <div className='min-h-[555px] flex overflow-hidden'>
+            <div className='min-h-[540px] overflow-hidden flex justify-between items-center'>
+                <div className='w-[48%]'>
+                    <div>
+                        <h1 className='text-5xl mb-[20px] font-black text-[#40196d]'>
+                            We're the bank of the free
+                        </h1>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
